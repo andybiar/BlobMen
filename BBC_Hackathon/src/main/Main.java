6f274c078b5bb10e9bae64bfdd8e006c383bb352
Andy Biar <sointhulted@yahoo.com>
2012-07-01 11:47:40 -0500

    checkpoint 11:47 am

--- BBC_Hackathon/src/main/Main.java
+++ BBC_Hackathon/src/main/Main.java
@@ -6,14 +6,18 @@ import fisica.*;
 public class Main extends PApplet {
 	public static final long serialVersionUID = 1L;
 	public final PApplet applet = this;
+	public final FWorld world = new FWorld();
 	
 	@Override
 	public void setup(){
 	size(200,200);
+	frameRate(60);
+	Fisica.init(this);
+	world.setEdges();
 	}
 	
 	@Override
 	public void draw(){
-		
+		world.step();
 	}
 }
