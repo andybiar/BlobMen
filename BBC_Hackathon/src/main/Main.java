97b90536dc6ab4d9132e6bfca465fcafb3804dad
DasBrieger <jwbrieger@gmail.com>
2012-07-01 11:48:36 -0500

    Stuff

--- BBC_Hackathon/src/main/Main.java
+++ BBC_Hackathon/src/main/Main.java
@@ -5,6 +5,7 @@ import processing.core.*;
 public class Main extends PApplet {
 	public static final long serialVersionUID = 1L;
 	public final PApplet applet = this;
+	
 	public void setup(){
 	size(200,200);
 	}
